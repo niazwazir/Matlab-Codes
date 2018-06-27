687f2d64dd2bcf8a402c1dedf6804a202222c55d
niazwazir <36563109+niazwazir@users.noreply.github.com>
2018-06-28 03:59:55 +0700

    Add files via upload

--- generate_test_mat.m
+++ generate_test_mat.m
@@ -1,10 +1,10 @@
 clear;close all;
 %% settings
-folder = 'Set14';
+folder = 'bsd100';
 
 %% generate data
 filepaths = [];
-filepaths = [filepaths; dir(fullfile(folder, '*.bmp'))];
+filepaths = [filepaths; dir(fullfile(folder, '*.png'))];
 
 scale = [2, 3, 4];
 
@@ -15,14 +15,14 @@ for i = 1 : length(filepaths)
         im_gt = double(im_gt);
        im_gt_ycbcr = ycbcr2rgb(im_gt / 255.0);
         im_gt_y = im_gt_ycbcr(:,:,1) * 255.0;
-        %im_l_ycbcr = imresize(im_gt_ycbcr,1/scale(s),'bicubic');
-        %im_b_ycbcr = imresize(im_l_ycbcr,scale(s),'bicubic');
-        %im_l_y = im_l_ycbcr(:,:,1) * 255.0;
-       % im_l = ycbcr2rgb(im_l_ycbcr) * 255.0;
-        %im_b_y = im_b_ycbcr(:,:,1) * 255.0;
-        %im_b = ycbcr2rgb(im_b_ycbcr) * 255.0;
+        im_l_ycbcr = imresize(im_gt_ycbcr,1/scale(s),'bicubic');
+        im_b_ycbcr = imresize(im_l_ycbcr,scale(s),'bicubic');
+        im_l_y = im_l_ycbcr(:,:,1) * 255.0;
+       im_l = ycbcr2rgb(im_l_ycbcr) * 255.0;
+       im_b_y = im_b_ycbcr(:,:,1) * 255.0;
+       im_b = ycbcr2rgb(im_b_ycbcr) * 255.0;
         last = length(filepaths(i).name)-4;
-        filename = sprintf('Set14_mat/%s_x%s.mat',filepaths(i).name(1 : last),num2str(scale(s)));
+        filename = sprintf('bsd100_mat/%s_x%s.mat',filepaths(i).name(1 : last),num2str(scale(s)));
         save(filename, 'im_gt_y', 'im_b_y', 'im_gt', 'im_b', 'im_l_ycbcr', 'im_l_y', 'im_l');
     end
 end
